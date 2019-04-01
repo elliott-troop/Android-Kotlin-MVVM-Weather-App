0622ad2706da7b5da52abe371e2bcae4920bb999
Elliott Porter Jr <elliott.porter@ipsoft.com>
2019-03-31 21:53:39 -0400

    Added all libraries needed for this application, created fragments for current weather, future weather, and settings, bottom navigation, and added navigation editor

---
+++ app/src/main/java/com/elliott/forecastmvvm/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.elliott.forecastmvvm.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel;
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
